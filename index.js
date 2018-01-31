67aa9190a79a2d819ca915cdc3b8481f4da3b54e
Michelle Romero <marhdz0@gmail.com>
2018-01-30 22:31:35 -0600

    Database

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express   = require('express');
-const bodyParser= require('body-parser');
-//const request = require('request-promise');
-const fs = require('fs');
 
-const orden=require('./orden.js')
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
+var {mongoose} = require('./server/db/mongoose');
+var {Orden} = require('./server/models/orden');
+
+//var app = express();
+//const port = process.env.PORT || 3000;
+
+//app.use(bodyParser.json());
+//******************************************************
 const server = express();
 server.use(bodyParser.urlencoded({
    extended: true
@@ -13,15 +19,25 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/lista-orden', function (req, res) {
-    var compra=req.body.result && req.body.result.parameters && req.body.result.parameters.Compra;
-    let id=req.body.sessionId;
-    console.log(id);
     var action=req.body.result.action;
-    var pedido=[];
+    // var pedido=[];
+    var orden = new Orden({
+      UserId: req.body.sessionId,
+      producto: req.body.result.parameters.Compra,
+       // precio:
+       // confirmado:
+       // enviado:
+    });
 
-    if(action==='AgregarCompra'){
-      var addOrden=orden.agregarOrden(id,compra);
-      return  res.json({
+      if(action==='AgregarCompra'){
+
+          orden.save().then((doc) => {
+            res.send(doc);
+          }, (e) => {
+            res.status(400).send(e);
+          });
+
+          return  res.json({
               speech: "",
               messages: [
                 {
@@ -47,22 +63,24 @@ server.post('/lista-orden', function (req, res) {
     };
 
     if(action==='VerCompra'){
-      var lista=orden.leerOrden(id);
-      console.log(lista);
-      listaPedidos= lista.map((x)=>{
-        return {
-          type:0,
-          platform: 'facebook',
-          speech:x.compra}
-      })
-    return  res.json({
-        speech: '',
-        messages: listaPedidos,
-        source: 'first-webhook'
+      Orden.find({'confirmado':false},function(e,docs){
+        listaPedidos= docs.map((x)=>{
+          return {
+            type:1,
+            platform: 'facebook',
+            imageUrl: 'https://i.imgur.com/VUEGlFp.jpg',
+            title :x.producto
+          }
+        });
+      return  res.json({
+          speech: '',
+          messages: listaPedidos,
+          source: 'first-webhook'
+        });
+
+      }, (e) => {
+        res.status(400).send(e);
       });
-
-      //return listaPedidos;
-
     };
 
     if (action==='EliminarOrden'){
@@ -107,3 +125,4 @@ server.post('/lista-orden', function (req, res) {
 server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and running...");
 });
+module.exports = {server};
---
+++ server/models/orden.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var Orden = mongoose.model('Orden', {
+  UserId: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  producto:{
+    type: String,
+    required:true
+  },
+  // precio:{
+  //   type: Number
+  // },
+  confirmado: {
+    type: Boolean,
+    default: false
+  },
+  enviado: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = {Orden};
