23945aa6eeb82666804c483a391a54f791b68071
Michelle Romero <marhdz0@gmail.com>
2018-02-01 20:05:13 -0600

    Precio desde API.AI

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ server.post('/lista-orden', function (req, res) {
     var orden = new Orden({
       UserId: req.body.sessionId,
       producto: req.body.result.parameters.Compra,
-       // precio:
+      precio: req.body.result.parameters.Precio
        // confirmado:
        // enviado:
     });
@@ -71,7 +71,7 @@ server.post('/lista-orden', function (req, res) {
             return {
               type:1,
               platform: 'facebook',
-              title :nombre+' ($'+productos[nombre].precio+')',
+              title :nombre+' ($'+x.precio+')',
               subtitle: productos[nombre].descripcion,
               imageUrl: productos[nombre].url,
               buttons:[{
@@ -81,7 +81,7 @@ server.post('/lista-orden', function (req, res) {
             }
         });
 
-        var listaPrecios=docs.map((x)=>{return parseFloat(productos[x.producto].precio);});
+        var listaPrecios=docs.map((x)=>{return parseFloat(x.precio);});
         var total=0;
         for(var i in listaPrecios) { total += listaPrecios[i];}
 
